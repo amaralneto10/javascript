1bdcd8f60e399fbd60429f71ef27bbaf194fede7
Amaral Neto <alunoamaralneto@gmail.com>
2024-12-31 15:03:43 -0300

    Projeto efetuado

--- exercicios/aula12ex01/modelo/script.js
+++ exercicios/aula12ex01/modelo/script.js
@@ -7,12 +7,15 @@ function carregar() {
 
     if (hora >= 0 && hora < 12) {
         // BOM DIA
+        document.body.style.background = '#89CEF8'
         img.src = 'manha.png'
     } else if (hora >= 12 && hora <= 18) {
         // BOA TARDE
+        document.body.style.background = '#EE6F04'
         img.src = 'tarde.png'
     } else {
         // BOA NOITE
+        document.body.style.background = '#032241'
         img.src = 'noite.png'
     }
 
