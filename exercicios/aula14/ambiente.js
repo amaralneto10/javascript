f31ae3a5015dc390363c8d914ac88aa78e4ca6b5
Amaral Neto <alunoamaralneto@gmail.com>
2025-02-08 12:56:00 -0300

    Incluído a função for

--- exercicios/aula14/ambiente.js
+++ exercicios/aula14/ambiente.js
@@ -1,5 +1,10 @@
-var c = 1
+/* var c = 1
 while (c <= 10) {
     console.log(`Número ${c}`)
     c++
-}
+} */
+
+    //-------------------------
+    for( var c = 1; c <= 10; c++) {
+        console.log(`Olá número ${c}`)
+    }
