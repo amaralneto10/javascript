dc9625eb40cfb7ef0ea6fc25637efef4f420bf28
Amaral Neto <alunoamaralneto@gmail.com>
2025-02-09 17:08:49 -0300

    Criado exercicios

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,24 +1,33 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} Horas.`
+function contar() {
+    let ini = document.getElementById("txti")
+    let fim = document.getElementById("txtf")
+    let passo = document.getElementById("txtp")
+    let res = document.getElementById('resultado')
 
-    if (hora >= 0 && hora < 12) {
-        // BOM DIA
-        document.body.style.background = '#89CEF8'
-        img.src = 'manha.png'
-    } else if (hora >= 12 && hora <= 18) {
-        // BOA TARDE
-        document.body.style.background = '#EE6F04'
-        img.src = 'tarde.png'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+       // window.alert('[ERRO] Faltam Dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
-        // BOA NOITE
-        document.body.style.background = '#032241'
-        img.src = 'noite.png'
-    }
+        res.innerHTML = ('Contando: ')
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+        }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `${c} \u{1F449} `
 
+    } 
 }
-
-
